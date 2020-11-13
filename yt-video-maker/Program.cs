54c863a85a436a75d9a377a6af2bc26144d3beaa
Bruno Sansigolo <bruno@sansigolo.com>
2020-11-13 16:29:56 +0000

    first commit: add VS project

---
+++ yt-video-maker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace yt_video_maker
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("My Hello World!");
+        }
+    }
+}
